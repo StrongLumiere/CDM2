032bdf53608c6631d673173160292fe2a163cba8
StrongLumiere <119430195+StrongLumiere@users.noreply.github.com>
2024-12-19 22:34:00 +0900

    main.py

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ tab1, tab2, tab3 = st.tabs(["Overview", "Procedure", "Hospitalization Prediction
 with tab1:
     display_tab1()
 
-with tab2:
-    st.markdown("### CSV 파일 업로드")
-    uploaded_file = st.file_uploader("CSV 파일을 업로드해주세요", type=["csv"], key="file_uploader_tab2")
-    # uploaded_file = pd.read_csv('file.csv')
-    display_tab2(uploaded_file)
+# with tab2:
+#     st.markdown("### CSV 파일 업로드")
+#     uploaded_file = st.file_uploader("CSV 파일을 업로드해주세요", type=["csv"], key="file_uploader_tab2")
+#     # uploaded_file = pd.read_csv('file.csv')
+#     display_tab2(uploaded_file)
 
    
-# 탭 3: Hospitalization Prediction
-with tab3:
-    st.markdown("### CSV 파일 업로드")
-    uploaded_file = st.file_uploader("CSV 파일을 업로드해주세요", type=["csv"], key="file_uploader_tab3")
-    # uploaded_file = pd.read_csv('file.csv')
-    display_tab3(uploaded_file)
+# # 탭 3: Hospitalization Prediction
+# with tab3:
+#     st.markdown("### CSV 파일 업로드")
+#     uploaded_file = st.file_uploader("CSV 파일을 업로드해주세요", type=["csv"], key="file_uploader_tab3")
+#     # uploaded_file = pd.read_csv('file.csv')
+#     display_tab3(uploaded_file)
     
     
